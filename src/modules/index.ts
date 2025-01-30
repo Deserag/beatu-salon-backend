d99e482afd9f7428aae30d729f377848ec70c4af
Deserag <pavelkopytov472@gmail.com>
2025-01-30 13:15:54 +0300

    Оставшиеся правки

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -4,6 +4,5 @@ export * from './auth'
 export * from './client'
 export * from './record'
 export * from './service'
-
 //Используем общее DTO для корректного получения любого значения
 export * from './dto'
--- src/modules/user/dto/index.ts
+++ src/modules/user/dto/index.ts
@@ -1,7 +1,6 @@
 export * from './create-department-user.dto';
 export * from './create-department.dto';
 export * from './create-role.dto';
-export * from './create-service.dto';
 export * from './create-user.dto';
 export * from './get-department.dto';
 export * from './get-role.dto'
